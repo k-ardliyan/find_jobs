8e3494838d4120ce788ac8ef4e4961f9c5fec705
k-ardliyan <k.ardliyan@gmail.com>
2022-08-08 15:04:33 +0700

    onboarding page is done

--- lib/pages/onboarding_page.dart
+++ lib/pages/onboarding_page.dart
@@ -1,10 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:find_jobs/theme.dart';
 
 class OnboardingPage extends StatelessWidget {
   const OnboardingPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Widget backgroundImage() {
+      return Image.asset(
+        'assets/onboarding.png',
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        fit: BoxFit.cover,
+      );
+    }
+
+    Widget content() {
+      return SafeArea(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 50),
+              Text(
+                'Build Your Next\nFuture Career Like\na Master',
+                style: whiteTextStyle.copyWith(fontSize: 32),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                '18,000 jobs available',
+                style: whiteTextStyle.copyWith(fontWeight: light),
+              ),
+              const Spacer(),
+              Center(
+                child: Container(
+                  width: 200,
+                  height: 45,
+                  child: TextButton(
+                    onPressed: () => {
+                      Navigator.pushNamed(context, '/sign-up'),
+                    },
+                    style: TextButton.styleFrom(
+                      backgroundColor: whiteColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(66),
+                      ),
+                    ),
+                    child: Text(
+                      'Get Started',
+                      style: purpleTextStyle.copyWith(fontWeight: medium),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              Center(
+                child: Container(
+                  width: 200,
+                  height: 45,
+                  child: TextButton(
+                    onPressed: () => {
+                      Navigator.pushNamed(context, '/sign-in'),
+                    },
+                    style: TextButton.styleFrom(
+                      backgroundColor: transparentColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(66),
+                        side: BorderSide(
+                          color: whiteColor,
+                        ),
+                      ),
+                    ),
+                    child: Text(
+                      'Sign In',
+                      style: whiteTextStyle.copyWith(fontWeight: medium),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 50),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          backgroundImage(),
+          content(),
+        ],
+      ),
+    );
   }
 }
