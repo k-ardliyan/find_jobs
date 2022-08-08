0e50a718c23cf8045e03529b09cdf8339cf854a4
k-ardliyan <k.ardliyan@gmail.com>
2022-08-08 14:27:23 +0700

    add assets & splash screen

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,10 +1,35 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:find_jobs/theme.dart';
+
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      const Duration(seconds: 3),
+      () => Navigator.of(context).pushReplacementNamed('/onboarding'),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: primaryColor,
+      body: Center(
+        child: Image.asset(
+          'assets/image_splash.png',
+          width: 178,
+        ),
+      ),
+    );
   }
 }
